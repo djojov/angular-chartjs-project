f58f6b13706f3163087fc25a1176f8e8cd8d4e51
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2023-08-18 13:35:48 +0200

    Add styles and create sidebar component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SidebarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ frontend/src/app/components/index.ts
@@ -0,0 +1,3 @@
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+export { SidebarComponent };
